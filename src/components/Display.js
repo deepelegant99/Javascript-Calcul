5b212d34c73cded97dbdc34694505ea87dc8a4f5
Phillip Wong <deepelegant99@gmail.com>
2022-04-30 22:28:24 -0700

    Added the testing code from freecodecamp

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,6 @@
 import displayStyles from "./Display.module.css";
 
 const Display = () => {
-  return <div className={displayStyles.display}>Here is the Display</div>;
+  return <div id='display' className={displayStyles.display}>Here is the Display</div>;
 };
 export default Display;
